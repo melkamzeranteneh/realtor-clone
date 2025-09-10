6ae6e4517a7e7ef1d7b268e7eb24ee5301954ed3
Melkamzer Anteneh <melkamzerantish@gmail.com>
2025-09-10 11:17:56 +0300

    Addedheader for the pages

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import ForgotPassword from "./pages/ForgotPassword";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/offers" element={<Offers/>}/>
-        <Route path="/sign-in" element={<SignIn/>}/>
-        <Route path="/sign-up" element={<SignUp/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/forgot-password" element={<ForgotPassword/>}/>
-      </Routes>
+      <Header>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/offers" element={<Offers/>}/>
+          <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        </Routes>
+      </Header>
     </Router>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {useLocation} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
+export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  function pathMatchRoute(route) {
+    if(route === location.pathname) {
+      return true;
+    }
+  }
+
+  return (
+    <div className='bg-white border-b shadow-sm sticky top-0 z-40'>
+      <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
+        <div>
+          <img src="https://static.rdc.moveaws.com/rdc-ui/logos/logo-brand.svg" alt="logo" className='h-5 cursor-pointer' onClick={()=>navigate("/")}></img>
+        </div>
+        <div>
+          <ul className='flex space-x-10'>
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] ${pathMatchRoute("/") && 'text-black border-b-red-500'}`} onClick={()=>navigate("/")}>
+            Home</li>
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] ${pathMatchRoute("/offers") && 'text-black border-b-red-500'}`} onClick={()=>navigate("/offers")}>
+            Offers</li>
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] ${pathMatchRoute("/sign-in") && 'text-black border-b-red-500'}`} onClick={()=>navigate("/sign-in")}>
+            Sign In</li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  )
+}
